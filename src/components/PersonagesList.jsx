628bd547ac17abd74348cc1c752fe3e66970c760
Jennifer <jenniferromanmuerte@gmail.com>
2025-12-02 17:48:25 +0100

    added message if no character is found with the input value

--- src/components/PersonagesList.jsx
+++ src/components/PersonagesList.jsx
@@ -1,24 +1,27 @@
 import PersonageCard from "./PersonageCard";
 
 const PersonagesList = ({ personages, filterName, filterHouse }) => {
+  const filtered = personages
+    .filter((personage) => personage.name.toLowerCase().includes(filterName))
+    .filter((personage) => personage.house === filterHouse);
+
   return (
     <ul className="personagesList">
-      {personages
-        .filter((personage) =>
-          personage.name.toLowerCase().includes(filterName)
-        )
-        .filter((personage) =>
-          personage.house === filterHouse
-        )
-        .map((personage) => (
+      {filtered.length === 0 ? (
+        <li>
+          No hay ningún personaje que coincida con la palabra {filterName}
+        </li>
+      ) : (
+        filtered.map((personage) => (
           <PersonageCard
             key={personage.id}
-            id = {personage.id}
+            id={personage.id}
             image={personage.images}
             name={personage.name}
             specie={personage.species}
           />
-        ))}
+        ))
+      )}
     </ul>
   );
 };
