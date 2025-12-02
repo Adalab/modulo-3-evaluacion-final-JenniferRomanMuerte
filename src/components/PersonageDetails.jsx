9c08da40083505cd35ff8fb32cfa1839a5d12921
Jennifer <jenniferromanmuerte@gmail.com>
2025-12-02 20:15:04 +0100

    Added styles in  PersonageDetails

--- src/components/PersonageDetails.jsx
+++ src/components/PersonageDetails.jsx
@@ -1,4 +1,5 @@
 import {useParams } from "react-router-dom";
+import "../styles/PersonageDetails.scss";
 
 const PersonageDetails = ({personages}) => {
  const { id } = useParams();
@@ -9,17 +10,17 @@ const PersonageDetails = ({personages}) => {
     <main className="mainDetails">
       <section className="mainDetails__card">
         <div  className="mainDetails__card--containerImg">
-          <img
+          <img  className="mainDetails__card--containerImg--img"
           src= {personageDetails.images}
           alt={personageDetails.name}
           />
         </div>
         <div className="mainDetails__card--info">
-          <span>{personageDetails.name}</span>
-          <span>Status:{personageDetails.alive ? 'Vivo': 'Muerto'}</span>
-          <span>Specie:{personageDetails.species}</span>
-          <span>Gender:{personageDetails.gender}</span>
-          <span>House:{personageDetails.house}</span>
+          <span className="mainDetails__card--info--name">{personageDetails.name}</span>
+          <span>Status: {personageDetails.alive ? 'Vivo': 'Muerto'}</span>
+          <span>Specie: {personageDetails.species}</span>
+          <span>Gender: {personageDetails.gender}</span>
+          <span>House: {personageDetails.house}</span>
         </div>
       </section>
     </main>
