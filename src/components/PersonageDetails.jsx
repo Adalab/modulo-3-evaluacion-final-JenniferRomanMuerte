8715ee3fc6fc4e96e44b66a61dd2444befb74eb2
Jennifer <jenniferromanmuerte@gmail.com>
2025-12-03 11:41:53 +0100

    Added coats of arms for each house in PersonageDetails

--- src/components/PersonageDetails.jsx
+++ src/components/PersonageDetails.jsx
@@ -1,25 +1,57 @@
-import {useParams } from "react-router-dom";
-import "../styles/PersonageDetails.scss";
+import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import "../styles/PersonageDetails.scss";
+import gryffindorImg from "../images/Gryffindor.png";
+import slytherinImg from "../images/Slytherin.png";
+import hufflepuffImg from "../images/Hufflepuff.png";
+import ravenclawImg from "../images/Ravenclaw.png";
 
-const PersonageDetails = ({personages}) => {
- const { id } = useParams();
+const PersonageDetails = ({ personages }) => {
+  const { id } = useParams();
 
- const personageDetails = personages.find(personage => personage.id === id);
- console.log(personageDetails);
+  const personageDetails = personages.find((personage) => personage.id === id);
+  let imgHouse = "";
+  if (personageDetails.house === "Gryffindor") {
+    imgHouse = gryffindorImg;
+  } else if (personageDetails.house === "Slytherin") {
+    imgHouse = slytherinImg;
+  } else if (personageDetails.house === "Hufflepuff") {
+    imgHouse = hufflepuffImg;
+  } else if (personageDetails.house === "Ravenclaw") {
+    imgHouse = ravenclawImg;
+  }
+
   return (
     <main className="mainDetails">
-    <Link className="mainDetails__link"  to= "/">Volver</Link>
+      <Link className="mainDetails__link" to="/">
+        Volver
+      </Link>
       <section className="mainDetails__card">
-        <div  className="mainDetails__card--containerImg">
-          <img  className="mainDetails__card--containerImg--img"
-          src= {personageDetails.images ? personageDetails.images :  "https://placehold.co/210x295/887c0e/666666/?format=svg&text=Harry+Potter"}
-          alt={personageDetails.name}
+        <div className="mainDetails__card--containerImg">
+          <img
+            className="mainDetails__card--containerImg--img"
+            src={
+              personageDetails.images
+                ? personageDetails.images
+                : "https://placehold.co/210x295/887c0e/666666/?format=svg&text=Harry+Potter"
+            }
+            alt={personageDetails.name}
           />
         </div>
         <div className="mainDetails__card--info">
-          <span className="mainDetails__card--info--name">{personageDetails.name}</span>
-          <span>Status: {personageDetails.alive ? 'Vivo': 'Muerto'}</span>
+          <div className="mainDetails__card--info--name" >
+            <div className="mainDetails__card--info--name--containerImg">
+              <img
+                className="mainDetails__card--info--name--containerImg--img"
+                src={imgHouse}
+                alt={personageDetails.house}
+              />
+            </div>
+            <span className="mainDetails__card--info--name--span">
+              {personageDetails.name}
+            </span>
+          </div>
+          <span>Status: {personageDetails.alive ? "Vivo" : "Muerto"}</span>
           <span>Specie: {personageDetails.species}</span>
           <span>Gender: {personageDetails.gender}</span>
           <span>House: {personageDetails.house}</span>
